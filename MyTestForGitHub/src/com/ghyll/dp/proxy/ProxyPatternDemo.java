7069b873a9667b1bcd78f5ad62e8a037be61add0
george.zhao <georgezhenzhao@gmail.com>
2015-11-10 17:16:08 -0500

    minor changes

--- MyTestForGitHub/src/com/ghyll/dp/proxy/ProxyPatternDemo.java
+++ MyTestForGitHub/src/com/ghyll/dp/proxy/ProxyPatternDemo.java
@@ -4,10 +4,11 @@ package com.ghyll.dp.proxy;
 public class ProxyPatternDemo {
 	
 	public static void main(String[] args) {
-		Image image = new ProxyImage("test.jpg");
+		Image image = new ProxyImage("proxy.jpg");
 
 		// image will be loaded from disk
 		image.display();
+		
 		System.out.println("");
 
 		// image will not be loaded from disk
--- MyTestForGitHub/src/com/ghyll/dp/proxy/RealImage.java
+++ MyTestForGitHub/src/com/ghyll/dp/proxy/RealImage.java
@@ -6,18 +6,18 @@ public class RealImage implements Image {
 	private String fileName;
 
 	public RealImage(String fileName) {
-		this.fileName = fileName;
 		loadFromDisk(fileName);
+		this.fileName = fileName;
+	}
+	
+	private void loadFromDisk(String fileName) {
+		System.out.println("Loading RealImage " + fileName);
 	}
 
 	@Override
 	public void display() {
 		System.out.println("real display");
 		System.out.println("Displaying RealImage " + fileName);
-	}
-
-	private void loadFromDisk(String fileName) {
-		System.out.println("Loading RealImage " + fileName);
 	}
 
 }
